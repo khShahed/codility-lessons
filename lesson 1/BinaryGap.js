f0ba69d8d60e20cded6469bd2600bd82dc549e20
Kamrul Hasan Shahed <khshahed90@gmail.com>
2019-05-12 04:04:06 +0600

    Comment refactored

--- lesson 1/BinaryGap.js
+++ lesson 1/BinaryGap.js
@@ -2,7 +2,7 @@
  * Problem link: https://app.codility.com/programmers/lessons/1-iterations/binary_gap/
  *
  *
- * Description: The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
+ * This solution scored 100%.
  */
 
 
